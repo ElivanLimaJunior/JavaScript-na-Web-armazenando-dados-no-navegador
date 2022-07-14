fdff57abe318d35789fb87e70fe173636598b39f
Elivan-Jr <elivanlimajunior@gmail.com>
2022-07-14 19:55:12 -0300

    capturando o target pelos elementos

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ form.addEventListener("submit", (evento) => {
     evento.preventDefault() // Cancela o evento se for cancelável, sem parar a propagação do mesmo. Irá manter o resultado do evento previnindo o comportamento padrão do evento.
 
     console.log(evento)
-    console.log(evento.target[0].value) // pegando elementos pelo target
-    console.log(evento.target[1].value)
+    console.log(evento.target.elements['nome'].value) // pegando pelos elementos do target
+    console.log(evento.target.elements['quantidade'].value) // é melhor dessa forma pois podemos futuramente acrescentar + elementos na lista.
 })
